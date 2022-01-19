61c6fba9f216e81ac7470707a5436dae3f331ccd
cares0 <cares00000@gmail.com>
2022-01-19 12:37:08 +0900

    Start

---
+++ src/com/cares/s1/start/StartMain.java
@@ -0,0 +1,15 @@
+package com.cares.s1.start;
+
+public class StartMain {
+
+	public static void main(String[] args) {
+		System.out.println("DB 연동 테스트 시작");
+		
+		
+		
+		
+		
+		System.out.println("DB 연동 테스트 끝");
+	}
+
+}
